efdead1eb29cf391986c4b4f400e4f4d7d87e62f
miyamo2 <miyamo2@outlook.com>
2024-07-23 07:30:49 +0900

    impl

---
+++ src/pages/404.tsx
@@ -0,0 +1,23 @@
+import { navigate } from "gatsby";
+import { Heading, useMediaQuery, Text, Button } from "@yamada-ui/react";
+import * as React from "react";
+import { Layout } from "@/components/Layout";
+
+const NotFound = () => {
+    const [isLarge] = useMediaQuery([
+        "(min-width: 1280px)",
+    ])
+    return (
+        <Layout scroll={false} isLarge={isLarge}>
+            <main>
+                <Heading className={"text-black text-3xl font-bold"} paddingBottom={"md"}>Page Not Found</Heading>
+                <Text>Oops! This page has been removed or relocated.</Text>
+                <Button onClick={() => navigate(-1)}>
+                    Go Back
+                </Button>
+            </main>
+        </Layout>
+    )
+}
+
+export default NotFound
