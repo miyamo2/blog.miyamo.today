6eccfeddbd5ef78ad366801d14b4c7f97adc23fb
miyamo2 <miyamo2@outlook.com>
2025-01-11 12:41:19 +0900

    feature: asynchronous buymeacoffee script

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -38,6 +38,7 @@ export const onRenderBody: GatsbySSR["onRenderBody"] = ({ setHeadComponents, set
   ]);
   setPostBodyComponents([
     <script
+      async={true}
       key="bmcWidget"
       data-name="BMC-Widget"
       src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
