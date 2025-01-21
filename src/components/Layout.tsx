1d08a2492faa52a943044906a0435c6220c2775f
miyamo2 <miyamo2@outlook.com>
2025-01-21 20:53:09 +0900

    Revert "hotfix: scroll"
    This reverts commit c19af2cba520836e7bb2425a013ee917bd2f155c.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -13,7 +13,7 @@ const Layout = ({ children }: LayoutProps) => {
     <div id={"layout"} className={"smooth-scroll"}>
       <Box h={"70px"} w={"full"} bg={["#f6f8fa", "#010409"]} className={"header"}>
         <div className={"flex w-full justify-center"} >
-          <div className={"max-w-[1400px] w-full lg:w-[95vw] mx-4 lg:mx-0"}>
+          <div className={"max-w-[1400px] w-full lg:w-[90vw] mx-4 lg:mx-0"}>
             <Header />
           </div>
         </div>
@@ -21,22 +21,37 @@ const Layout = ({ children }: LayoutProps) => {
       <Grid
         id={"content"}
         w={"full"}
+        h={"100vh"}
         className={"scrollbar-offset"}
         templateAreas={`
-          "m"
-          "f"`}
+          "h h h h h h h h h"
+          "m m m m m m m m m"
+          "m m m m m m m m m"
+          "m m m m m m m m m"
+          "m m m m m m m m m"
+          "m m m m m m m m m"
+          "m m m m m m m m m"
+          "f f f f f f f f f"`}
         bg={["#ffffff", "#0d1117"]}
       >
+        <GridItem gridArea={"h"} h={"70px"} bg={["#f6f8fa", "#010409"]} className={"header"} alignSelf={"start"}>
+          <div className={"flex w-full justify-center"} >
+            <div className={"max-w-[1400px] w-full lg:w-[90vw] mx-4 lg:mx-0"}>
+              <Header />
+            </div>
+          </div>
+        </GridItem>
         <GridItem
+          h={"full"}
           gridArea={"m"}
         >
-          <div className={"flex w-full justify-center"}>
-            <div className={"max-w-[1400px] w-full lg:w-[95vw] mx-5 lg:mx-0 pb-[64px] lg:pb-0"}>
+          <div className={"flex w-full h-full justify-center"}>
+            <div className={"max-w-[1400px] w-full h-full lg:w-[95vw] mx-5 lg:mx-0 pb-[64px] lg:pb-0"}>
               {children}
             </div>
           </div>
         </GridItem>
-        <GridItem w={"full"} gridArea={"f"} alignSelf={"end"}>
+        <GridItem w={"full"} h={"full"} gridArea={"f"} alignSelf={"end"}>
           <Footer />
         </GridItem>
       </Grid>
